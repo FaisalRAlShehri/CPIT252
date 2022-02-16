7de7db61f6ffb3d0703a64333e6a7106334c9c05
Faisal Al-Shehri <61276435+FaisalRAlShehri@users.noreply.github.com>
2022-02-16 23:59:16 +0300

    Add files via upload

---
+++ Lab02/src/lab02/Lab02.java
@@ -0,0 +1,19 @@
+package lab02;
+
+/**
+ *
+ * @author Faisal
+ */
+public class Lab02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Order o1 = new Order();
+        Shipment sh1 = new Shipment("Ahmed",
+                "248 NE. Pleasant St. Niceville, FL 32578",
+                "123-477-0001");
+    }
+
+}
